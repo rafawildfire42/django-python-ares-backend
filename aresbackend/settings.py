a04fb0925adf115de9e46ef93ec7b6af9c0b34e7
rafaelwildfire <rafaelwildfire@gmail.com>
2023-08-06 01:16:07 -0300

    fix: ajuste cors

--- aresbackend/settings.py
+++ aresbackend/settings.py
@@ -76,10 +76,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'aresbackend.urls'
 
 CORS_ALLOWED_ORIGINS = [
-    'https://www.aresprojetos.com.br',  
-    'https://aresprojetos.com.br',  
-    'https://backend.aresprojetos.com.br',  
-    'https://www.backend.aresprojetos.com.br',  
+    '*'
 ]
 
 CORS_ALLOW_METHODS = [
