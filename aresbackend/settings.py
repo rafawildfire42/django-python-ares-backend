1ea9cb5090e81d84da04b952bd5285a6bc3a4249
rafaelwildfire <rafaelwildfire@gmail.com>
2023-08-06 19:03:33 -0300

    fix: cors

--- aresbackend/settings.py
+++ aresbackend/settings.py
@@ -76,15 +76,17 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'aresbackend.urls'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+    'http://localhost:3001',
     'https://www.aresprojetos.com.br',
     'https://backend.aresprojetos.com.br',
-    # Adicione outras origens permitidas, se necessário
 ]
-
-
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3001',
+    'https://www.aresprojetos.com.br',
+    'https://backend.aresprojetos.com.br',
+)
 CORS_ALLOWED_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
-CORS_ALLOWED_HEADERS = ['Authorization', 'Content-Type']
+CORS_ALLOWED_HEADERS = ['Authorization', 'Content-Type', 'Access-Control-Allow-Origin']
 CORS_ALLOW_CREDENTIALS = True
 
 TEMPLATES = [
