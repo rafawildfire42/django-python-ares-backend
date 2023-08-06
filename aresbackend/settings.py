919c44d003e7fd095552990be1bf99a05ecc566d
rafaelwildfire <rafaelwildfire@gmail.com>
2023-08-06 01:17:19 -0300

    fix: teste

--- aresbackend/settings.py
+++ aresbackend/settings.py
@@ -76,7 +76,11 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'aresbackend.urls'
 
 CORS_ALLOWED_ORIGINS = [
-    '*'
+    'https://www.aresprojetos.com.br',  
+    'https://aresprojetos.com.br',  
+    'https://backend.aresprojetos.com.br',  
+    'http://backend.aresprojetos.com.br',  
+    'https://www.backend.aresprojetos.com.br',  
 ]
 
 CORS_ALLOW_METHODS = [
